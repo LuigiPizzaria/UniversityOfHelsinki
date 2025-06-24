4de14be83b7d74ac29195dffbd47ae089702ac92
Luigi Pizzaria <luizajandarian@yahoo.com>
2025-06-24 11:19:10 +0200

    part2 phonebook 2.9*

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
 
   //meant for controlling the input field for a new name
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -15,6 +19,11 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value)
+    //filter the persons array based on the search term
   }
 
   const addPhonebookEntry = (event) =>{
@@ -29,19 +38,32 @@ const App = () => {
     if(newName === '' || newNumber === '') {
       return alert('Name or number cannot be empty')
     }
+
+    //multiple people can share a phone number if they share a household so only check for duplicate names
     if(persons.some(person => person.name === newName)) {
       setNewName('')
       setNewNumber('')
       return alert(`${newName} is already added to phonebook`)
     }
+
     setPersons(persons.concat(phonebookObject))
     setNewName('')
     setNewNumber('')
   }
 
+  //convert the serach term and the names to lowercase for case-insensitive search
+  //according to google every person hold one empty string in the name field so empty string is not an issue
+  const filteredPersons = persons.filter(person => 
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  )
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        Search for name: <input value={searchTerm} onChange={handleSearchChange} />
+      </div>
+      <h2>Add new</h2>
       <form onSubmit={addPhonebookEntry}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -55,9 +77,13 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) =>
-          <p key={person.id}>{person.name} {person.number}</p>
-        )}
+        {filteredPersons.length === 0 ? (
+          <p>No entries found</p>
+        ) : (filteredPersons.map(person =>
+          <p key={person.id}>
+            {person.name} {person.number}
+          </p>
+        ))}
       </div>
     </div>
   )
