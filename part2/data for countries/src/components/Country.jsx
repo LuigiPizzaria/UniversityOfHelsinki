889b6ea78eb8be86d27db695b1c99b66b9a3530d
Luigi Pizzaria <luizajandarian@yahoo.com>
2025-09-03 23:51:10 +0200

    exercise 2.20 data for countries fixed

--- part2/data for countries/src/components/Country.jsx
+++ part2/data for countries/src/components/Country.jsx
@@ -5,12 +5,15 @@ const Country = ({country}) => {
     const [weather, setWeather] = useState(null)
     const api_key = import.meta.env.VITE_SOME_KEY
 
+    console.log(country)
+
     //fetch weather data from api every time the country prop changes (component is rendered with a different country)
     useEffect(() => {
         axios
-            .get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${api_key}`)
+            .get(`https://api.openweathermap.org/data/2.5/weather?lat=${country.capitalInfo.latlng[0]}&lon=${country.capitalInfo.latlng[1]}&appid=${api_key}`)
             .then(response => {
                 setWeather(response.data)
+                console.log(response.data)
             })
     }, [country])
 
@@ -31,6 +34,7 @@ const Country = ({country}) => {
                 <h2>Weather in {country.capital}</h2>
                 <p>Temperature {kelvinToCelsius(weather.main.temp)}</p>
                 <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="weather icon" />
+                <p>Wind {weather.wind.speed} m/s</p>
                 </div>
             ) : (<p>Loading weather...</p>)}
             
