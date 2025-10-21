a083d8c4b17af71b42566094f19e7c9ced710956
Luigi Pizzaria <luizajandarian@yahoo.com>
2025-10-21 10:17:48 +0200

    exercise 3.8

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -3,7 +3,8 @@ const app = express()
 const morgan = require('morgan')
 
 app.use(express.json()) //for handling JSON in POSR requests
-app.use(morgan('tiny')) //for logging HTTP requests to the console
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body')) //for logging HTTP requests to the console
 
 
 let persons = [
